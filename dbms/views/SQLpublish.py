099e30eb9c901f5e4c62706cd4c39247bea21135
thinker <996846239@qq.com>
2017-07-04 22:12:20 +0800

    Update SQLpublish.py
    修复pt执行多条语句获取进度问题

--- dbms/views/SQLpublish.py
+++ dbms/views/SQLpublish.py
@@ -308,12 +308,12 @@ class SqlProgress(View):
         time.sleep(2)
         sql_hash_str = request.GET.get('sql_hash', None)
         sql_hash_list = sql_hash_str.split('*')[1:]
-        result_dict = {'ptosc_flag': 1}
-        print(sql_hash_list)
+        result_dict = {'ptosc_flag': 0, 'wid_status': 1, 'wid': ''}
+        # wid_status: 1=audit, 2=running, 3=over
+        sql_hash_list = set(sql_hash_list)
         if not sql_hash_list:
-            result_dict['ptosc_flag'] = 0
             return HttpResponse(json.dumps(result_dict))
-        # result_dict = {'id': {'per': '', 'time_consuming': ''}}
+        # result_dict = {'wid':'', 'per': '', 'time_consuming': '', 'ptosc_flag': 0, 'wid_status': 1}
         # 需要查询sql_hash 对应工单的状态，如果是执行中，则获取进度，如果为执行结束，则返回 100， 如果未执行，则返回0
         for sql_hash in sql_hash_list:
             sql_status = models.InceptionAuditDetail.objects.filter(sql_hash='*'+sql_hash).values('flag')
@@ -326,21 +326,19 @@ class SqlProgress(View):
 
                 result = conn.conn_query("inception get osc_percent '%s'" % '*'+sql_hash)
                 if result:
-                    result_dict[sql_hash] = {}
-                    result_dict[sql_hash]['per'] = result[0][3]
-                    result_dict[sql_hash]['time_consuming'] = str(result[0][4])
+                    result_dict['wid'] = sql_hash
+                    result_dict['per'] = result[0][3]
+                    result_dict['time_consuming'] = str(result[0][4])
                 else:
-                    if sql_hash in result_dict.items():
-                        result_dict[sql_hash]['per'] = 100
-                        result_dict[sql_hash]['time_consuming'] = 0
+                    if sql_hash == result_dict['wid']:
+                        result_dict['per'] = 100
+                        result_dict['time_consuming'] = 0
             else:
-                result_dict[sql_hash] = {}
-                result_dict[sql_hash]['per'] = 100
-                result_dict[sql_hash]['time_consuming'] = 0
+                result_dict['wid'] = sql_hash
+                result_dict['per'] = 100
+                result_dict['time_consuming'] = 0
 
-        print(result_dict)
-        return HttpResponse('ok')
-        # return HttpResponse(json.dumps(result_dict))
+        return HttpResponse(json.dumps(result_dict))
 
     def post(self, request, wid):
         user_info = GetUserInfo(request)
